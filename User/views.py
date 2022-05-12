3e47b05e7bd5c924ab92ba12a24c282ea396533a
SammyJesuyon <sammyjesuyon@yahoo.com>
2022-05-12 13:49:47 +0100

    done

--- User/views.py
+++ User/views.py
@@ -1,7 +1,6 @@
 from .serializers import (User, UserSerializer)
 from rest_framework.viewsets import ModelViewSet
 
-
 class UserView(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
